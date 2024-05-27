cd51807e668f710e7c29aaecaad3c9f3c4eb8dc0
Quang Dong <quangdongne88@gmail.com>
2024-05-27 17:03:27 +0700

    Final commit

---
+++ src/config/gemini.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-undef */
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = "YOUR_API_KEY";
+const MODEL_NAME = "gemini-1.0-pro";
+async function run(prompt) {
+  const genAI = new GoogleGenerativeAI(apiKey);
+
+  const model = genAI.getGenerativeModel({
+    model: MODEL_NAME,
+  });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topP: 1,
+    topK: 0,
+    maxOutputTokens: 2048,
+    responseMimeType: "text/plain",
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chatSession = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+  return response.text();
+}
+
+export default run;
